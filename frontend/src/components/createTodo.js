19b090e064aec0f7187e945e872276c79dfcc8ca
cstew527 <christopherstewart05@gmail.com>
2022-12-06 11:01:34 -0500

    Added card

--- frontend/src/components/createTodo.js
+++ frontend/src/components/createTodo.js
@@ -3,7 +3,22 @@ import {useState} from 'react'
 const CreateTodo = (props) => {
 
     return (
-        <h1>HTML for the create page goes here</h1>
+        <>
+            <div class="card">
+                <div class="card-bidy">
+                    <h5 class="card-title">{props.task}</h5>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="completed"/>
+                        <label class="form-check-label" for="completed">
+                            Completed
+                        </label>
+                    </div>
+                    <div class="btn-group" role="group">
+                        <button type="button" class="btn btn-primary">Edit</button>
+                        <button type="button" class="btn btn-primary">Delete</button>
+                    </div>
+                </div>
+            </div>
+        </>
     )
-
 }
